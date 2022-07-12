bbbb0eff5bfffaa322e367ef12dabb9b8dab1d58
Ramires <moreira.ramires@gmail.com>
2022-07-12 10:56:44 -0300

    search movie

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import MovieList from './components/movieList';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async () => {
-		const url = `http://www.omdbapi.com/?s=lord of the rings&apikey=e3f459ca`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=e3f459ca`;
 
-        // http://www.omdbapi.com/?s=lord of the rings&apikey=e3f459ca
+
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -20,15 +23,19 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		getMovieRequest();
-	}, []);
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
 	return (
 		<div className='container-fluid movie-app'>
-			<div className='row'>
-				<MovieList movies={movies} />
-			</div>
+		<div className='row d-flex align-items-center mt-4 mb-4'>
+			<MovieListHeading heading='Movies' />
+			<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 		</div>
+		<div className='row'>
+			<MovieList movies={movies} />
+		</div>
+	</div>
 	);
 };
 
